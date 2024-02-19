6300e91df2ba53d8300162d11a3c31349950ea55
Tran Hoang Nhat <kundeeptryvl@gmail.com>
2024-02-19 12:19:51 +0700

    update img, html, js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
 const navElements = document.querySelectorAll('a[href^="#"');
+const navbar = document.querySelector('.navbar');
+const buttons = document.querySelectorAll('button');
+const itemHasMenuEls = document.querySelectorAll('.noclick');
+const navMenuItems = document.querySelectorAll('.nav-menu-item');
 
 //animation smooth
 navElements.forEach(anchor => {
@@ -10,4 +14,50 @@ navElements.forEach(anchor => {
         behavior: 'smooth'
       });
     });
+});
+
+//fixed navbar when scrolling
+document.addEventListener('DOMContentLoaded', function () {
+  const navbarHeight = navbar.clientHeight;
+  const position = navbar.clientTop + navbar.clientHeight + 150;
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > position) {
+      navbar.classList.add('fixed');
+      navbar.style.height = '10%';
+    } else if (window.scrollY < position) {
+      navbar.classList.remove('fixed');
+      navbar.style.height = navbarHeight + 'px';
+    }
+  })
+});
+
+
+// navigate to /none.html for buttons has no functionality
+buttons.forEach(function (button) {
+  let noneURL = document.URL + 'none.html';
+
+  button.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.open(noneURL);
+  })
+});
+
+
+//remove default animation of nav-item has list
+itemHasMenuEls.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+  })
+});
+
+//fix scope nav-item in nav-menu
+navMenuItems.forEach(function (item) {
+  const navigatedURL = 
+  item.querySelector('a').href;
+
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.open(navigatedURL);
+  })
 });
